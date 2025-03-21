9e70cb79495ce8b427d019da917c8a2f6b4f2eb1
johny11111 <125285264+johny11111@users.noreply.github.com>
2025-03-21 18:06:11 +0200

    api

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,5 +1,5 @@
-//const API_URL = 'https://taskmanager-server-ygfb.onrender.com/api/users';
-const API_URL = 'http://localhost:5000/api/users';
+const API_URL = 'https://taskmanager-server-ygfb.onrender.com/api/users';
+//const API_URL = 'http://localhost:5000/api/users';
 
 
 export const registerUser = async (userData) => {
