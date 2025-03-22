79a7a8ea76c08f01dc0d78e1ede057953cb023be
johny11111 <125285264+johny11111@users.noreply.github.com>
2025-03-22 03:33:16 +0200

    teampage

--- src/pages/TeamsPage.jsx
+++ src/pages/TeamsPage.jsx
@@ -26,7 +26,7 @@ const TeamsPage = () => {
     
         try {
             const token = localStorage.getItem('token');
-          const response = fetch('https://taskmanager-server.onrender.com/api/users/create', {
+          const response = await fetch('https://taskmanager-server.onrender.com/api/users/create', {
 
                 method: 'POST',
                 headers: {
