330215561c56ddd301a659c70c4de2a435df40aa
johny11111 <125285264+johny11111@users.noreply.github.com>
2025-03-22 03:04:15 +0200

    context

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.jsx'
+import { UserProvider } from './context/UserContext.js';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  
+     <>
+    <UserProvider>
+      <App />
+    </UserProvider>
+  
+  </>,
 )
