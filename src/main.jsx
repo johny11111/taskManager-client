ffe439ac8abf5b38a27ce13637b1de36cce63f27
johny11111 <125285264+johny11111@users.noreply.github.com>
2025-03-22 03:10:56 +0200

    context1

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.jsx'
-import { UserProvider } from './context/UserContext.js';
+import { UserProvider } from './context/UserContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   
